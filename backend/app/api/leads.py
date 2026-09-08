9acc459f58e1f117dbf680ea7e7c08dad22e7b4a
Abhishek6969-69 <sbh123yadav@gmail.com>
2026-09-08 10:33:39 +0530

    database and orm setup done

--- backend/app/api/leads.py
+++ backend/app/api/leads.py
@@ -1,21 +1,49 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from app.database.connection import SessionLocal
+from app.database.models import LeadDB
 from app.models.lead import Lead
 
 router = APIRouter()
 
-leads = []
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @router.post("/leads")
-def create_lead(lead: Lead):
-    leads.append(lead)
+def create_lead(lead: Lead, db: Session = Depends(get_db)):
+    new_lead = LeadDB(
+        name=lead.name,
+        email=lead.email,
+        company=lead.company,
+        website=lead.website,
+        job_title=lead.job_title
+    )
+
+    db.add(new_lead)
+    db.commit()
+    db.refresh(new_lead)
 
     return {
         "message": "Lead created successfully",
-        "lead": lead
+        "lead": {
+            "id": new_lead.id,
+            "name": new_lead.name,
+            "email": new_lead.email,
+            "company": new_lead.company,
+            "website": new_lead.website,
+            "job_title": new_lead.job_title
+        }
     }
 
 
 @router.get("/leads")
-def get_leads():
-    return leads
+def get_leads(db: Session = Depends(get_db)):
+    return db.query(LeadDB).all()
